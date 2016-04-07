9016f3691aabb9c2127c6dfd04c90df377d6c870
jianghouwei <279941737@qq.com>
2016-04-07 21:20:22 +0800

    12312321

--- README.md
+++
@@ -1,4 +0,0 @@
-# SunDay
-Spring maven mybatis  swfupload
-
-//url http://localhost:8080/SunDay/jsp/jj.jsp
--- src/main/resources/mydays.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `days_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `state` varchar(255) DEFAULT NULL,
-  `nickname` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
